1808d8f90792a569fa348bb02be34e7d2340473a
Vikas Vashisth <vikas.vashisth@knoldus.com>
2021-03-08 22:23:33 +0530

    Uploading the Assignment

---
+++ Lambda Practice - Assignment_5/src/main/java/com/koldus/Driverclass.java
@@ -0,0 +1,17 @@
+package com.koldus;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*Main Class to call the Function*/
+public class Driverclass {
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<>(Arrays.asList("java", "scala", "java", "clojure", "clojure"));
+        Distinct_String_impl distinct_string_impl = new Distinct_String_impl();
+        List<String> result= distinct_string_impl.returndistinctstring.implementofDistinctFromString(list1);
+        System.out.println(result);
+    }
+}
+
+
