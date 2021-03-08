2142af57126ad863f80290f75b0c880213631d72
VikasVashisth <76785155+VikasVashisth@users.noreply.github.com>
2021-03-08 22:31:17 +0530

    Delete Lambda Practice - Assignment_5 directory

--- Lambda Practice - Assignment_5/src/main/java/com/koldus/Distinct_String.java
+++
@@ -1,9 +0,0 @@
-package com.koldus;
-
-import java.util.ArrayList;
-import java.util.List;
-/*Created Functional Interface*/
-public interface Distinct_String {
-
-    public List<String> implementofDistinctFromString(ArrayList<String> listOfStrings);
-}
