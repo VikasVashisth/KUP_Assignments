fa335c68d922086c688a3292b810dfd46d799192
VikasVashisth <76785155+VikasVashisth@users.noreply.github.com>
2021-03-08 22:30:25 +0530

    Delete Assignment_2 directory

--- Assignment_2/src/main/java/lambdas/GeneratePrimesInRangeInterface.java
+++
@@ -1,7 +0,0 @@
-package lambdas;
-import java.util.ArrayList;
-import java.util.List;
-
-public interface GeneratePrimesInRangeInterface {
-    List<Integer>  filterOutPrimes(ArrayList<Integer> inputNumbers);
-}
